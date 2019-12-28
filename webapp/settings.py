be83410cda1a9f07cd01ca794f226cad42ca30cd
raghibmrz <zcabrmi@ucl.ac.uk>
2019-12-28 19:56:13 +0000

    deploying

--- webapp/settings.py
+++ webapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'y+oee&-miw(q&jh%xt_89t)t!-lt@q)&a@+ji6qfh$x6)v*o%s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
